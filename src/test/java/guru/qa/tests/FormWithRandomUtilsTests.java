571f9d98d1e3cd1ee733f1ae7e44a86224ffd68c
Alexander Bogdanov <alexander.bogdanov@nutson.us>
2022-01-23 20:29:53 +0300

    random utils added

--- src/test/java/guru/qa/tests/FormWithRandomUtilsTests.java
+++ src/test/java/guru/qa/tests/FormWithRandomUtilsTests.java
@@ -1,24 +1,28 @@
 package guru.qa.tests;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static guru.qa.utils.RandomUtils.*;
 
 
-public class FormTests extends TestBase {
+public class FormWithRandomUtilsTests extends TestBase {
 
+    String firstName = getRandomString(12);
+    String email = getRandomEmail();
+    String phoneNumber = getRandomPhone();
     @Test
     public void successfulFormFillTest() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        $("#firstName").setValue("Alex");
+        $("#firstName").setValue(firstName);
         $("#lastName").setValue("Bogdanov");
-        $("#userEmail").setValue("alex@mail.com");
+        $("#userEmail").setValue(email);
         $("#genterWrapper").$(byText("Other")).click();
-        $("#userNumber").setValue("7911999781");
+        $("#userNumber").setValue(phoneNumber);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1977");
@@ -34,9 +38,9 @@ public class FormTests extends TestBase {
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-//        $(".table-responsive").shouldHave(text("Alex Bogdanov"), text("alex@mail.com"));
-        $(".table-responsive").$(byText("Student Name"))
-                .parent().shouldHave(text("Alex Bogdanov"));
+        $(".table-responsive").shouldHave(text(firstName + " " + "Bogdanov"), text(email));
+//        $(".table-responsive").$(byText("Student Name"))
+//                .parent().shouldHave(text("Alex Bogdanov"));
     }
 
 }
